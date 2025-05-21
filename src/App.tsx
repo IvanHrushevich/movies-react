ba903bdbabbd2cb785a8c3c256764041bf61263b
Ivan Grushevich <ivan.grushevich1@gmail.com>
2025-05-21 15:55:11 +0300

    feat: add images, add some basic markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 const App = () => {
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">App</h1>
-        </div>
+        <main>
+            <div className="pattern" />
+            <div className="wrapper">
+                <header>
+                    <img src="./hero.png" alt="Hero banner" />
+                    <h1>
+                        Find <span className="text-gradient">Movies</span>{' '}
+                        You'll Enjoy Without the Hassle
+                    </h1>
+                </header>
+                <p>Search </p>
+            </div>
+        </main>
     );
 };
 
